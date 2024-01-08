c0d38307f613414630e9d99a9805c78ca1617de8
SamComerford93 <samcomerford93@gmail.com>
2024-01-08 09:24:54 +0000

    Practiced assigning variables, string concatenation and using expressions in JS.

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,27 @@
-document.write('Hello, World!');
+document.write('Hello, World!'); // Printing Hello World 
+
+var A = "This is a string"; // Assigning a string to a variable
+
+document.write(A); // Printing previous variable
+
+var x = "This message will pop up in a window!"; // Assigning a String to a variable
+
+window.alert(x); // Printing previous variable in a pop up window
+
+document.write("Sam said \"Are you going to help me?\". Sean replied \"I'm not!\""); // Using quotes within a string
+
+document.write("My name is " + "Sam!"); // Concatenating a string
+
+var C = "This sentence has been " + "concatenated and assinged to" + " a variable."; // Assigning a concatenated string to a variable
+
+document.write(C); // Printing previous variable
+
+var Dog = "Henry", Cat = "Molly", Hamster = "Fred", Rabbit = "Roger"; // Assigning multiple variables 
+
+document.write(Cat); // Prinitng one of the variables from previous task
+
+document.write(5 + 2); // Printing the result of the expression
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string." // Created two variables that both conatin a string
+
+document.write(Sent1 + Sent2) // Concatenated both strings together and printed them
