cbfb579509ea3b778c933082130fc0ddd4e5fc6d
SamComerford93 <samcomerford93@gmail.com>
2024-01-24 15:28:45 +0000

    Updated JS comments

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,4 +1,4 @@
-// Connecting multiple strings using the .concat() method
+// Connecting multiple strings using the concat() method
 function make_sentence() {
     var part_1 = "This sentence ";
     var part_2 = "has been ";
@@ -8,7 +8,7 @@ function make_sentence() {
     document.getElementById("concatenate").innerHTML = whole_sentence;
 }
 
-// Using the slice.() method to display a section of a string
+// Using the slice() method to display a section of a string
 function string_slice() {
     var my_string = "Congratulations to Christoper for winning the prize!";
     var winner_name = my_string.slice(19,29);
@@ -22,32 +22,32 @@ function all_caps() {
     document.getElementById("upper").innerHTML = shout_hb;
 }
 
-// Using the .search() to search a string
+// Using the search() to search a string
 function search_string() {
     var string_1 = "The party will be held on Wednesday!";
     var is_in_string = string_1.search("Wednesday");
     document.getElementById("string_check").innerHTML = is_in_string;
 }
 
-// Using the .toString() method
+// Using the toString() method
 function string_method() {
     var num = 50;
     document.getElementById("num_to_string").innerHTML = num.toString();
 }
 
-// Using the .toPrecision() method
+// Using the toPrecision() method
 function precision_method() {
     var long_num = 12734.1983473564;
     document.getElementById("precise").innerHTML = long_num.toPrecision(10);
 }
 
-// Using the .toFixed() method
+// Using the toFixed() method
 function fixed_method() {
     var decimal_num = 3.6789;
     document.getElementById("decimal_places").innerHTML = decimal_num.toFixed(2);
 }
 
-// Using the .valueOf() method
+// Using the valueOf() method
 function value_method() {
     var string = "Hello World!";
     document.getElementById("primitive").innerHTML = string.valueOf();
